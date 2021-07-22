1a0932e7550be7184fe082860d889b4801e86a44
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-07-22 21:13:30 +0800

    sql

---
+++ sql/mysql/create-tables.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS album;
+CREATE TABLE album (
+  id         INT AUTO_INCREMENT NOT NULL,
+  title      VARCHAR(128) NOT NULL,
+  artist     VARCHAR(255) NOT NULL,
+  price      DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO album 
+  (title, artist, price) 
+VALUES 
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
